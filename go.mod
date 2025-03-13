81e270860a78047b12be356a0dbba2dffa16ccb2
Migara Mewantha <predator@localhost.localdomain>
2025-03-13 09:57:42 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module rbac.go
+
+go 1.23.5
---
+++ rbac.go
@@ -0,0 +1,22 @@
+package main
+
+import "slices"
+
+var roles = map[string][]string{
+	"admin":    {"manage_users", "manage_order", "manage_products", "view_reports"},
+	"seller":   {"create_product", "edit_products", "delete_product", "view_orders"},
+	"customer": {"view_products", "place_order"},
+}
+
+type User struct {
+	Username string
+	Role     string
+}
+
+func (u User) GetPermissions() []string {
+	return roles[u.Role]
+}
+
+func HasPermission(user User, action string) bool {
+	return slices.Contains(user.GetPermissions(), action)
+}
